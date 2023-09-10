a640091d5b1919da81945e09032b14e62efe35ce
Levit Osei-Wusu <lkanner21@gmail.com>
2023-09-10 21:07:09 +0000

    modify category model

--- Entities/Models/Category.cs
+++ Entities/Models/Category.cs
@@ -1,11 +1,9 @@
-using GraphNet.Entities.Models;
-
-namespace GraphNet.Entities;
+namespace GraphNet.Entities.Models;
 
 public class Category
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public ICollection<Product> Products { get; set; }
+    public ICollection<Product> Products { get; set; } = new List<Product>();
 }
