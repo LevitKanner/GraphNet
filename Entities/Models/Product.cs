c2e1430c2e55827a91c010240c777fa4f7a58469
Levit Osei-Wusu <lkanner21@gmail.com>
2023-09-10 21:10:41 +0000

    modify product model

--- Entities/Models/Product.cs
+++ Entities/Models/Product.cs
@@ -5,7 +5,8 @@ namespace GraphNet.Entities.Models;
 public class Product
 {
     public int Id { get; set; }
-    public int Name { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public int CategoryId { get; set; }
 }
