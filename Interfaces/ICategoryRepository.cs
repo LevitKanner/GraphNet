2fe78d7235f6bb6ff215a25886dcdb1cc002ad39
Levit Osei-Wusu <lkanner21@gmail.com>
2023-09-10 21:08:02 +0000

    add category repository interface

--- Interfaces/ICategoryRepository.cs
+++ Interfaces/ICategoryRepository.cs
@@ -1,6 +1,12 @@
+using GraphNet.Entities;
+using GraphNet.Entities.Models;
+
 namespace GraphNet.Interfaces;
 
 public interface ICategoryRepository
 {
-    
+    IEnumerable<Category> GetAllCategoriesAsync();
+    Task<Category?> GetCategoryById(int id);
+    Task<Category> UpdateCategoryAsync(int id, Category update);
+    Task<Category> DeleteCategoryAsync(int id);
 }
