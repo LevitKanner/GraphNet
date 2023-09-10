e63f7b96454775223d09a46799fdf3da62efa582
Levit Osei-Wusu <lkanner21@gmail.com>
2023-09-10 21:32:38 +0000

    add Iservicecollection extensions

--- Extensions/ServiceExtensions.cs
+++ Extensions/ServiceExtensions.cs
@@ -1,3 +1,5 @@
+using GraphNet.Interfaces;
+using GraphNet.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace GraphNet.Extensions;
@@ -17,9 +19,14 @@ public static class ServiceExtensions
 
     public static void ConfigureDb(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContextPool<ApplicationContext>(
+        services.AddPooledDbContextFactory<ApplicationContext>(
             builder => builder.UseNpgsql(configuration.GetConnectionString("DbConnection"))
                 .EnableSensitiveDataLogging()
                 .UseSnakeCaseNamingConvention());
+    }
+
+    public static void ConfigureInjections(this IServiceCollection services)
+    {
+        services.AddTransient<ICategoryRepository, CategoryRepository>();
     }
 }
