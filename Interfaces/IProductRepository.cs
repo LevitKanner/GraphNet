b9e1fd145ce176876c4c8b737396c826d74725af
Levit Osei-Wusu <lkanner21@gmail.com>
2023-09-10 21:08:31 +0000

    add product repository interface

--- Interfaces/IProductRepository.cs
+++ Interfaces/IProductRepository.cs
@@ -1,6 +1,11 @@
+using GraphNet.Entities.Models;
+
 namespace GraphNet.Interfaces;
 
 public interface IProductRepository
 {
-    
+    IEnumerable<Product> GetAllProductsAsync();
+    Task<Product?> GetProductByIdAsync(int id);
+    Task<Product?> UpdateProduct(int id, Product product);
+    Product? DeleteProduct(int id);
 }
